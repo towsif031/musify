9e4082943b5a0e764f6475753b1787d4692f6cfa
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-25 16:10:56 +0600

    added email validation

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -27,15 +27,32 @@
 		}
 
 		private function validateFirstName($fn) {
-			
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
+				return;
+			}
 		}
 
 		private function validateLastName($ln) {
-			
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
+				return;
+			}
 		}
 
 		private function validateEmails($em, $em2) {
-			
+			if($em != $em2) {
+				array_push($this->errorArray, "Your emails don't match");
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, "Email is invalid");
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
 		}
 
 		private function validatePasswords($pw, $pw2) {
