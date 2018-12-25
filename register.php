65c541ad63e2f6422bc3591d40bd485a8e4e8004
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-25 11:55:48 +0600

    added account class

--- register.php
+++ register.php
@@ -1,50 +1,12 @@
-<?php 
+<?php
+	include("includes/classes/Account.php");
 
-function sanitizeFormPassword($inputText) {
-	$inputText = strip_tags($inputText);
-	return $inputText;
-}
+	$account = new Account();
+	$account->register();
 
-function sanitizeFormUsername($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	return $inputText;
-}
-
-function sanitizeFormString($inputText) {
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	$inputText = ucfirst(strtolower($inputText));
-	return $inputText;
-}
-
-if(isset($_POST['loginButton'])) {
-	//Login button was pressed
-	
-}
-
-if(isset($_POST['registerButton'])) {
-	//Register button was pressed
-	$username = sanitizeFormUsername($_POST['username']);
-	
-	$firstName = sanitizeFormString($_POST['firstName']);
-
-	$lastName = sanitizeFormString($_POST['lastName']);
-
-	$email = sanitizeFormString($_POST['email']);
-
-	$email2 = sanitizeFormString($_POST['email2']);
-
-	$password = sanitizeFormPassword($_POST['password']);
-
-	$password2 = sanitizeFormPassword($_POST['password2']);
-
-}
-
-
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
 ?>
-
-
 
 <html>
 <head>
