f54633e50aba842a8bf5b8866e513d3d83c7f576
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-27 18:31:22 +0600

    insert user details function added

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+	ob_start();
+
+	$timezone = date_default_timezone_set("Europe/London");
+
+	$con = mysqli_connect("localhost", "root", "", "slotify");
+
+	if(mysqli_connect_errno()) {
+		echo "Failed to connect: " . mysqli_connect_errno();
+	}
+?>
--- register.php
+++ register.php
@@ -1,8 +1,9 @@
 <?php
+	include("includes/config.php");
 	include("includes/classes/Account.php");
 	include("includes/classes/Constants.php");
 
-	$account = new Account();
+	$account = new Account($con);
 
 	include("includes/handlers/register-handler.php");
 	include("includes/handlers/login-handler.php");
