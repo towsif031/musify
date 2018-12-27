a70ecb9b6cffb3a69eab2283b0864b51170f346a
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-27 23:31:11 +0600

    added sessions

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -7,6 +7,7 @@ if(isset($_POST['loginButton'])) {
 	$result = $account->login($username, $password);
 
 	if($result == true) {
+		$_SESSION['userLoggedIn'] = $username;
 		header("Location: index.php");
 	}
 
