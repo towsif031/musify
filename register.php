9e468a5d35cbc077363a52e9d73214fd1bf905b0
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-30 23:47:47 +0600

    switch between login and register

--- register.php
+++ register.php
@@ -39,6 +39,10 @@
 					</p>
 
 					<button type="submit" name="loginButton">LOG IN</button>
+
+					<div class="hasAccountText">
+						<span id="hideLogin">Don't have an account yet? Signup here.</span>
+					</div>
 					
 				</form>
 
@@ -92,6 +96,10 @@
 					</p>
 
 					<button type="submit" name="registerButton">SIGN UP</button>
+
+					<div class="hasAccountText">
+						<span id="hideRegister">Already have an account? Log in here.</span>
+					</div>
 					
 				</form>
 
