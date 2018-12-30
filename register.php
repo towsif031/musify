c449eac663eaa608392c061231fe06f69cec03ff
Towsif Ahmed Omi <towsif031@gmail.com>
2018-12-31 01:21:02 +0600

    fixed switching

--- register.php
+++ register.php
@@ -18,7 +18,11 @@
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
+
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
 </head>
 <body>
 	<div id="background">
