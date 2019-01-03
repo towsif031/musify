2892ac853ba53e08b24288eb6a3aedd72c8e2ed3
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-03 12:07:18 +0600

    added right side content of login register page

--- register.php
+++ register.php
@@ -131,6 +131,17 @@
 
 
 			</div>
+
+			<div id="loginText">
+				<h1>Get great music, right now</h1>
+				<h2>Listen to loads of songs for free</h2>
+				<ul>
+					<li>Discover music you'll fall in love with</li>
+					<li>Create your own playlists</li>
+					<li>Follow artists to keep up to date</li>
+				</ul>
+			</div>
+
 		</div>
 	</div>
 
