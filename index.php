f120d141b44c418b143775f2fe8051960adc929d
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-03 18:52:25 +0600

    added progressbar background

--- index.php
+++ index.php
@@ -32,6 +32,56 @@ else {
 
 			<div id="nowPlayingCenter">
 
+				<div class="content playerControls">
+
+					<div class="buttons">
+
+						<button class="controlButton shuffle" title="Shuffle button">
+							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+						</button>
+
+						<button class="controlButton previous" title="Previous button">
+							<img src="assets/images/icons/previous.png" alt="Previous">
+						</button>
+
+						<button class="controlButton play" title="Play button">
+							<img src="assets/images/icons/play.png" alt="Play">
+						</button>
+
+						<button class="controlButton pause" title="Pause button" style="display: none;">
+							<img src="assets/images/icons/pause.png" alt="Pause">
+						</button>
+
+						<button class="controlButton next" title="Next button">
+							<img src="assets/images/icons/next.png" alt="Next">
+						</button>
+
+						<button class="controlButton repeat" title="Repeat button">
+							<img src="assets/images/icons/repeat.png" alt="Repeat">
+						</button>
+
+					</div>
+
+
+					<div class="playbackBar">
+
+						<span class="progressTime current">0.00</span>
+
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+
+						<span class="progressTime remaining">0.00</span>
+
+
+					</div>
+
+
+				</div>
+
+
 			</div>
 
 			<div id="nowPlayingRight">
