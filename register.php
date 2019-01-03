398ed4566e3995c844dbb0e8bc2be5d508dba230
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-03 12:51:20 +0600

    remember username if wrong password entered

--- register.php
+++ register.php
@@ -57,7 +57,7 @@
 					<p>
 						<?php echo $account->getError(Constants::$loginFailed); ?>
 						<label for="loginUsername">Username</label>
-						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('loginUsername') ?>" required>
 					</p>
 					<p>
 						<label for="loginPassword">Password</label>
