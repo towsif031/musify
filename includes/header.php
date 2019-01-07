b69ba3010c484742a1f606c95171bb1cd1a7f6c8
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-08 01:41:23 +0600

    added jquery

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,7 @@ else {
 
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="assets/js/script.js"></script>
 </head>
 
