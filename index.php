c846f330ed76c4158927b0c7ff368ca7ef97f772
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-07 17:39:03 +0600

    separated header and footer

--- index.php
+++ index.php
@@ -1,41 +1,5 @@
-<?php
-include("includes/config.php");
-
-//session_destroy(); LOGOUT
-
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-}
-else {
-	header("Location: register.php");
-}
-
-?>
-
-<html>
-<head>
-	<title>Welcome to Slotify!</title>
-
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-
-<body>
-
-	<div id="mainContainer">
-
-		<div id="topContainer">
-
-			<?php include("includes/navBarContainer.php"); ?>
-
-		</div>
-
-		<?php include("includes/nowPlayingBar.php"); ?>
-
-	</div>
-
-	
+<?php include("includes/header.php"); ?>
 
 
-</body>
 
-</html>
+<?php include("includes/footer.php"); ?>
