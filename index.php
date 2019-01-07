bf94af54bcbe70cf14a003e15875fc876702b4a8
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-07 19:36:49 +0600

    display album image

--- index.php
+++ index.php
@@ -5,10 +5,25 @@
 <div class="gridViewContainer">
 
 	<?php
-		$albumQuery = mysqli_query($con, "SELECT * FROM albums");
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
 		while($row = mysqli_fetch_array($albumQuery)) {
-			echo $row['title'] . "<br>";
+			
+
+
+
+			echo "<div class='gridViewItem'>
+
+					<img src='" . $row['artworkPath'] . "'>
+
+					<div class='gridViewInfo'>"
+						. $row['title'] .
+					"</div>
+
+				</div>";
+
+
+
 		}
 	?>
 
