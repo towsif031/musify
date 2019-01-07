b4a7d4011444b9a01a8c9850c552ca3fe94285f4
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-07 20:11:35 +0600

    added links to albums

--- index.php
+++ index.php
@@ -13,12 +13,13 @@
 
 
 			echo "<div class='gridViewItem'>
+					<a href='album.php?id=" . $row['id'] . "'>
+						<img src='" . $row['artworkPath'] . "'>
 
-					<img src='" . $row['artworkPath'] . "'>
-
-					<div class='gridViewInfo'>"
-						. $row['title'] .
-					"</div>
+						<div class='gridViewInfo'>"
+							. $row['title'] .
+						"</div>
+					</a>
 
 				</div>";
 
