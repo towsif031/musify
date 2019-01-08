828b83ab35ce3723209f89c785294242bd638d32
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-08 19:21:52 +0600

    update play count when a song is played

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -37,8 +37,8 @@ function setTrack(trackId, newPlaylist, play) {
 		});
 
 
-		audioElement.setTrack(track.path);
-		audioElement.play();
+		audioElement.setTrack(track);
+		playSong();
 	});
 
 	if(play == true) {
@@ -47,6 +47,11 @@ function setTrack(trackId, newPlaylist, play) {
 }
 
 function playSong() {
+
+	if(audioElement.audio.currentTime == 0) {
+		$.post("includes/handlers/ajax/updatePlays.php", { songId: audioElement.currentlyPlaying.id });
+	}
+
 	$(".controlButton.play").hide();
 	$(".controlButton.pause").show();
 	audioElement.play();
