7d772cf8ae48cc95cfb01187e752a3b2d991a7bf
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-08 19:56:11 +0600

    update progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,40 @@ var currentPlaylist = [];
 var audioElement;
 
 
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time / 60); //Rounds down
+	var seconds = time - (minutes * 60);
+
+	var extraZero = (seconds < 10) ? "0" : "";
+
+	return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+	$(".progressTime.current").text(formatTime(audio.currentTime));
+	$(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+	var progress = audio.currentTime / audio.duration * 100;
+	$(".playbackBar .progress").css("width", progress + "%");
+}
+
 function Audio() {
 
 	this.currentlyPlaying;
 	this.audio = document.createElement('audio');
+
+	this.audio.addEventListener("canplay", function() {
+		//'this' refers to the object that the event was called on
+		var duration = formatTime(this.duration);
+		$(".progressTime.remaining").text(duration);
+	});
+
+	this.audio.addEventListener("timeupdate", function(){
+		if(this.duration) {
+			updateTimeProgressBar(this);
+		}
+	});
 
 	this.setTrack = function(track) {
 		this.currentlyPlaying = track;
