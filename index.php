8b028a2c183ca71f4d3f25a18a877b41b5f1b695
Towsif Ahmed Omi <towsif031@gmail.com>
2019-01-13 21:49:23 +0600

    added browse page

--- index.php
+++ index.php
@@ -2,32 +2,6 @@
 include("includes/includedFiles.php"); 
 ?>
 
-<h1 class="pageHeadingBig">You Might Also Like</h1>
 
-<div class="gridViewContainer">
+<script>openPage("browse.php")</script>
 
-	<?php
-		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
-
-		while($row = mysqli_fetch_array($albumQuery)) {
-			
-
-
-
-			echo "<div class='gridViewItem'>
-					<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
-						<img src='" . $row['artworkPath'] . "'>
-
-						<div class='gridViewInfo'>"
-							. $row['title'] .
-						"</div>
-					</span>
-
-				</div>";
-
-
-
-		}
-	?>
-
-</div>
