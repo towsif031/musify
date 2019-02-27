b3e772e31f1d0c76c1e43e5351bee151251a8bb3
Towsif Ahmed Omi <towsif031@gmail.com>
2019-02-28 01:51:40 +0600

    added search page with functionality

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -9,16 +9,16 @@
 			$this->id = $id;
 		}
 
+		public function getId() {
+			return $this->id;
+		}
+
 		public function getName() {
 			$artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
 			$artist = mysqli_fetch_array($artistQuery);
 			return $artist['name'];
 		}
-
-		public function getId() {
-			return $this->id;
-		}
-
+		
 		public function getSongIds() {
 
 			$query = mysqli_query($this->con, "SELECT id FROM songs WHERE artist='$this->id' ORDER BY plays ASC");
