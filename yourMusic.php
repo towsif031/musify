9fa852cf7c2bf65db45a04703cb2f3f1e6c56269
Towsif Ahmed Omi <towsif031@gmail.com>
2019-03-05 00:57:11 +0600

    completed your music page

--- yourMusic.php
+++ yourMusic.php
@@ -26,7 +26,8 @@ include("includes/includedFiles.php");
 
 				$playlist = new Playlist($con, $row);
 
-				echo "<div class='gridViewItem'>
+				echo "<div class='gridViewItem' role='link' tabindex='0' 
+							onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
 
 						<div class='playlistImage'>
 							<img src='assets/images/icons/playlist.png'>
