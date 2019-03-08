d5ede3d8cc035921e269ba617422b44d53836a04
Towsif Ahmed Omi <towsif031@gmail.com>
2019-03-08 21:11:36 +0600

    delete playlist with ajax

--- playlist.php
+++ playlist.php
@@ -23,7 +23,7 @@ $owner = new User($con, $playlist->getOwner());
 		<h2><?php echo $playlist->getName(); ?></h2>
 		<p>By <?php echo $playlist->getOwner(); ?></p>
 		<p><?php echo $playlist->getNumberOfSongs(); ?> songs</p>
-		<button class="button" onclick="deletePlaylist(1)">DELETE PLAYLIST</button>
+		<button class="button" onclick="deletePlaylist('<?php echo $playlistId; ?>')">DELETE PLAYLIST</button>
 
 	</div>
 
