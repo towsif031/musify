5bd3fdcc1e47c1fd919d9dd67206985a1110c541
Towsif Ahmed Omi <towsif031@gmail.com>
2019-04-03 01:21:45 +0600

    added style to add to playlist dropdown menu

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -53,10 +53,18 @@
 
 		public static function getPlaylistsDropdown($con, $username) {
 			$dropdown = '<select class="item playlist">
-							<option value="">Add to playlist</option>
-						</select>';
+							<option value="">Add to playlist</option>';
 
-			return $dropdown;
+			$query = mysqli_query($con, "SELECT id, name FROM playlists WHERE owner='$username'");
+			while($row = mysqli_fetch_array($query)) {
+				$id = $row['id'];
+				$name = $row['name'];
+
+				$dropdown = $dropdown . "<option value='$id'>$name</option>";
+			}
+
+
+			return $dropdown . "</select>";
 		}
 
 
